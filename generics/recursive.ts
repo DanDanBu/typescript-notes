b98d39d82e7daeca6a6030e6a7dd723008ffbaa1
ChenChan-Huang <simba51082003@gmail.com>
2024-10-08 17:51:39 +0800

    Fix examples.

--- generics/recursive.ts
+++ generics/recursive.ts
@@ -1,6 +1,6 @@
 /* 
     type T1 = SnakeToCamelCase<‘this_is_snake_case’>; // “thisIsSnakeCase”
-    type T2 = type T2 = SnakeToCamelCase<‘This_Is_Strange_Case’>; // “thisIsSnakeCase”
+    type T2 = type T2 = SnakeToCamelCase<‘This_Is_Strange_Case’>; // “thisIsStrangeCase”
     type T3 = SnakeToCamelCase<‘IDontKnowThis’>; // “IDontKnowThis”
 */
 
